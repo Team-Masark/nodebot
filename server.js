234b3ef6884522640976853401427b63a059b723
Rahul Gaba <rahul_gaba@external.mckinsey.com>
2016-07-16 23:29:04 +0530

    variable definition fix

--- server.js
+++ server.js
@@ -5,46 +5,47 @@ const app = express()
 const Robot = require('./lib/robot')
 
 const nodebot = Robot({
-	mlPositive: 22,
-	mlNegative: 23,
-	mrPositive: 24,
-	mrNegative: 25
+  mlPositive: 22,
+  mlNegative: 23,
+  mrPositive: 24,
+  mrNegative: 25
 })
 
 const takePicture = require('./lib/take-picture')
 
-done = action => () => console.log('Robot : ', action)
+const done = action => () => console.log('Robot : ', action)
 
 app.use(express.static('public'))
 
 app.post('/robot/left', (req, res) => {
-	nodebot.moveLeft().then(done('left'))
-	res.end('ok')
+  nodebot.moveLeft().then(done('left'))
+  res.end('ok')
 })
 
 app.post('/robot/right', (req, res) => {
-	nodebot.moveRight().then(done('right'))
-	res.end('ok')
+  nodebot.moveRight().then(done('right'))
+  res.end('ok')
 })
 
 app.post('/robot/forward', (req, res) => {
-	nodebot.moveForward().then(done('right'))
-	res.end('ok')
+  nodebot.moveForward().then(done('right'))
+  res.end('ok')
 })
 
 app.post('/robot/backward', (req, res) => {
-	nodebot.moveBackward().then(done('backward'))
-	res.end('ok')
+  nodebot.moveBackward().then(done('backward'))
+  res.end('ok')
 })
 
 app.post('/robot/stop', (req, res) => {
-	nodebot.stop().then(done('stop'))
-	res.end('ok')
+  nodebot.stop().then(done
+('stop'))
+  res.end('ok')
 })
 
 app.post('/robot/picture', (req, res) => {
-	takePicture()
-	res.end('ok')
+  takePicture()
+  res.end('ok')
 })
 
 app.listen(3000, () => console.log('Server started on port 3000'))
